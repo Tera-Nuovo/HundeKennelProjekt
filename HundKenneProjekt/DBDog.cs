2115c88d9616e54ec71e38a879330b5d2676fb83
teis-ravn-jonsen <tera41930@edu.ucl.dk>
2022-09-07 21:33:05 +0900

    Update DBDog.cs

--- HundKenneProjekt/DBDog.cs
+++ HundKenneProjekt/DBDog.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 
 public class DBDog
 {
-	public DBDog()
-	{
-
-		Console.ReadKey();
-	}
+        [Name("navn")]
+        public string Name { get; set; }
+        [Name("ID")]
+        public string ID { get; set; }
+        [Name("sex")]
+        public string Sex { get; set; }
+        [Name("HD")]
+        public string Hips { get; set; }
+        [Name("HDindex")]
+        public string HDIndex { get; set; }
+        [Name("SP")]
+        public string backInfo { get; set; }
+        [Name("HZ")]
+        public string HeartInfo { get; set; }
+	
 }
