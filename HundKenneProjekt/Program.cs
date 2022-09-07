a64e29c1a60039ebab0b7fcc6757826d655d3a80
teis-ravn-jonsen <tera41930@edu.ucl.dk>
2022-09-07 21:02:48 +0900

    Update Program.cs

--- HundKenneProjekt/Program.cs
+++ HundKenneProjekt/Program.cs
@@ -4,8 +4,11 @@ namespace HundKenneProjekt
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            DogDBManager DBman = new DogDBManager();
+            List<Dog> testList = DBman.GetDogsFromDatabase();
 
             Console.ReadLine();
         }
