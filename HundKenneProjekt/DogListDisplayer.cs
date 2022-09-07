9baa7660dbc11bac31ec71889136686b468429af
Christian Bech Fruchard <cbfr38694@edu.ucl.dk>
2022-09-07 11:37:25 +0200

    DogListDisplayer class created

---
+++ HundKenneProjekt/DogListDisplayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class DogListDisplayer
+{
+	public DogListDisplayer()
+	{
+		List<int> doglist = new List<int>
+		{ 1, 2, 3, 4, 5, 6,};
+
+		Console.WriteLine(doglist);
+		Console.ReadKey();
+	}
+}
