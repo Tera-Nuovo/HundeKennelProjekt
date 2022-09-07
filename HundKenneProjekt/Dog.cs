ffee79ca8e8b9a4ca2afb81c7eedc11a159bc851
Robin Klyssing Jensen <rkje44183@edu.ucl.dk>
2022-09-07 11:27:30 +0200

    Dog Class Headers

--- HundKenneProjekt/Dog.cs
+++ HundKenneProjekt/Dog.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
 
 namespace HundKenneProjekt
 {
     public class Dog
     {
+        [Name("navn")]
         public string Name { get; set; }
+        [Name("ID")]
         public string ID { get; set; }
+        [Name("sex")]
         public string Sex { get; set; }
+        [Name("HD")]
         public string Hips { get; set; }
+        [Name("HDindex")]
         public string HDIndex { get; set; }
+        [Name("SP")]
         public int backInfo { get; set; }
+        [Name("HZ")]
         public int HeartInfo { get; set; }
     }
 }
