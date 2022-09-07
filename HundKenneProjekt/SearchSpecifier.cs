53f8ee0ba27b3e73d46b4b26e07d7eeaf13dbbb8
teis-ravn-jonsen <tera41930@edu.ucl.dk>
2022-09-07 21:56:37 +0900

    Update SearchSpecifier.cs

--- HundKenneProjekt/SearchSpecifier.cs
+++ HundKenneProjekt/SearchSpecifier.cs
@@ -1,10 +1,90 @@
 ﻿using System;
 namespace HundKenneProjekt
 {
-    public class SearchSpecifier
+    static public class SearchSpecifier
     {
-        public SearchSpecifier()
+        //Ikke færdig!
+        static public void RequestDogList()
         {
+
+        }
+
+        static private int GetMax()
+        {
+            string input;
+            int max;
+            Console.WriteLine("Specificer max værdi");
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out max))
+            {
+                return max;
+            } else
+            {
+                return -1;
+            }
+
+
+            
+        }
+
+        static private int GetMin()
+        {
+            string input;
+            int min;
+
+            Console.WriteLine("Specificer minimum værdi");
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out min))
+            {
+                return min;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static private string GetPriority()
+        {
+   
+            string priority;
+
+            Console.WriteLine("Specificer om listen skal sortes med højeste værdig øverst eller laveste værdi øverst.");
+
+            priority = Console.ReadLine();
+            if(priority == "hf")
+            {
+                return "hf";
+            } else
+            {
+                return "";
+            }
+
+
+
+
+        }
+
+        static private string GetCategori()
+        {
+            string categori;
+
+            Console.WriteLine("Specificer sorteingskategori");
+
+            categori = Console.ReadLine();
+
+            Console.WriteLine("Specificer om listen skal sortes med højeste værdig øverst eller laveste værdi øverst.");
+
+            if ( == "hf")
+            {
+                return "hf";
+            }
+
+
+
+
         }
     }
 }
