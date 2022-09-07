36e24f8dc155ed904e4928d3d873235083ffec7b
Robin Klyssing Jensen <rkje44183@edu.ucl.dk>
2022-09-07 11:00:13 +0200

    CSVHelper + DogVariables

--- HundKenneProjekt/Dog.cs
+++ HundKenneProjekt/Dog.cs
@@ -8,5 +8,12 @@ namespace HundKenneProjekt
 {
     public class Dog
     {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string Sex { get; set; }
+        public string Hips { get; set; }
+        public string HDIndex { get; set; }
+        public int backInfo { get; set; }
+        public int HeartInfo { get; set; }
     }
 }
