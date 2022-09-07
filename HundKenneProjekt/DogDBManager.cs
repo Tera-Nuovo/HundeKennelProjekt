880d450c655cdfde9121cd568a3efabd2fca30f6
Robin Klyssing Jensen <rkje44183@edu.ucl.dk>
2022-09-07 11:36:20 +0200

    Hunde database

---
+++ HundKenneProjekt/DogDBManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+
+namespace HundKenneProjekt
+{
+    public class DogDBManager
+    {
+        public List<Dog> GetDogsFromDatabase()
+        {
+            using (var streamReader = new StreamReader(@"\bin\Debug\HundeData.csv"))
+            {
+                var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = ";"
+                };
+                using (var csvReader = new CsvReader(streamReader, csvConfig))
+                {
+                    List<Dog> DogList = csvReader.GetRecords<Dog>().ToList();
+                    return DogList;
+                }
+            }
+        }
+    }
+}
