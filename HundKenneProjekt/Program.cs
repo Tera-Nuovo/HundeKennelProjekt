a6c3e6bf56c91314c91f3e6fbc6d4e83e9ad3d05
Christian Bech Fruchard <cbfr38694@edu.ucl.dk>
2022-09-08 12:59:47 +0200

    test

--- HundKenneProjekt/Program.cs
+++ HundKenneProjekt/Program.cs
@@ -10,10 +10,13 @@ namespace HundKenneProjekt
 
             mainMenu.RunMainMenu();
 
+            /*
             DogDBManager DBman = new DogDBManager();
             List<AbstractDog> testList = DBman.GetDogsFromDatabase();
             DogListDisplayer testDisplayer = new DogListDisplayer();
             testDisplayer.DisplayDogList(testList);
+            */
+
 
             Console.ReadLine();
         }
